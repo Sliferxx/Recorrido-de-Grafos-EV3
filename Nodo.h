5de94c20be66b49c1d6d98f54d7f9e5f5d904ecf
Sliferxx <kaioken20_70@live.com>
2015-09-09 18:55:45 -0300

    Update Nodo.h

--- Nodo.h
+++ Nodo.h
@@ -1,1 +1,18 @@
+#ifndef stdio_h
+#include <stdio.h>
+#define stdio_h
+#endif
 
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+typedef struct TNodo
+{
+  int dato;
+  int distancia;
+  int Tiempo_D;
+  int Tiempo_T;
+  char color;
+  char etiqueta
+}
